205ca9d1a79e450ce10c573780794aba5f1f4a3d
lyviaamorim <lyviacorreadeamorim@gmail.com>
2026-09-22 09:58:48 -0300

    feat: Criação de tipagem e json

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react"
+import type { TipoProduto } from "../../types/types";
+
+
 
 export default function Produtos() {
     // Para alterar o título da página:
     document.title = "Produtos"
+
+    const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+    useEffect(()=>{
+
+        const carregaProdutos = async ()=>{
+            
+            try {
+                const response = await fetch("http://localhost:3001/produtos");
+                
+                if(!response.ok){
+                        throw new Error("Erro na listagem dos produtos!")
+                }
+
+                const data:TipoProduto[] = await response.json();
+                console.log(data);
+
+
+            } catch (error) {
+                console.error(error);
+            }
+
+
+        }
+
+        carregaProdutos();
+
+    },[]);
+
+    //apresente a lista de produtos em CARDS ultilizando PROPS...
 
     return (
         <main>
---
+++ my-app/src/types/types.ts
@@ -0,0 +1,8 @@
+export type TipoProduto = {
+        id:string,
+        nome:string,
+        preco:number,
+        descricao:string,
+        avatar:string
+
+}
