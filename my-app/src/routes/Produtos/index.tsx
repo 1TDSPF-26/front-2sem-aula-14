37d95f614c83c07b18f8579272806d04dc5cfb5b
lenahlawi07 <lynnhalawi07@gmail.com>
2026-09-22 09:53:49 -0300

    feat : cria types e  utiliza o json produtos.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+import type { TipoProduto } from "../../types/types";
+
+
+
 export default function Produtos(){
     document.title = "Produtos"
+
+    const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+    useEffect( ()=>{
+
+        const carregaProdutos = async () => {
+
+            try {
+                const response = await fetch("http://localhost:3001/produtos");
+
+                if(!response.ok){
+                    throw new Error("Erro na listagem dos produtos");
+                }
+
+                const data: TipoProduto[] = await response.json();
+                console.log(data);
+
+            } catch (error){
+                console.error(error);
+            }
+            
+        }
+
+        carregaProdutos();
+
+    },[]);
+
     return(
         <main>
             <h2>Produtos</h2>
