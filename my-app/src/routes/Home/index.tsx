04ac2065a1ac3cf16dfe9d4a1dfb212d5964e5ca
gabriel <gabrielrazod9j@gmail.com>
2026-09-22 11:51:30 -0300

    feat: Realizar consumo de api externa.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,26 +1,6 @@
 import { useEffect, useState } from "react";
+import type { TipoUsuarioGit } from "../../types/types";
 // Tipagem TypeScript definindo o formato dos dados do usuário que virão da API
-type TipoUsuarioGit = {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  user_view_type: string;
-  site_admin: boolean;
-};
 
 export default function Home() {
   // Para alterar o título da página:
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react"
+import type { TipoProduto } from "../../types/types";
+
 export default function Produto() {
     //para alterar o titulo da pagina:
     document.title = "Produtos"
+
+     const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+    useEffect(() => {
+
+        const carregaProdutos = async () => {
+
+            try {
+                const response = await fetch("http://localhost:3001/produtos");
+
+                if (!response.ok) {
+                    throw new Error("Erro na listagem dos produtos!");
+                }
+
+                const data: TipoProduto[] = await response.json();
+                console.log(data);
+
+            } catch (error) {
+                console.error(error);
+            }
+
+        }
+
+        carregaProdutos();
+
+    }, []);
+
+
+    //Apresente a lista de produtos em CARDS utilizando PROPS...
+
     return (
         <main>
             <h2> Produtos</h2>
